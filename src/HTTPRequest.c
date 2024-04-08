3ce0caaa10285c01a185c26ad6cb6129f6b2ccf0
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2024-04-08 18:18:30 +0200

    imp parsing first line of http requests

--- src/HTTPRequest.c
+++ src/HTTPRequest.c
@@ -1,15 +1,12 @@
 #include "../includes/HTTPRequest.h"
 #include <string.h>
-#include <stdbool.h>
-#include <stdio.h>
 
 HTTPRequest* parseHTTP(const char* pRequestStr) {
     HTTPRequest* request = malloc(sizeof(HTTPRequest));
     request->raw = strdup(pRequestStr);
     request->headers = htCreate(10);
-    request->method = NULL;
-    request->path = NULL;
-    request->version = NULL;
+
+    // parse first line
 
     request->body = strstr(request->raw, "\r\n\r\n");
     if (request->body != NULL) {
@@ -17,6 +14,20 @@ HTTPRequest* parseHTTP(const char* pRequestStr) {
     }
 
     char* keyBegin = strstr(request->raw, "\r\n") + 2;
+
+    request->method = request->raw;
+    char* endOfToken = strstr(request->raw, " ");
+    endOfToken[0] = '\0';
+
+    request->path = endOfToken + 1;
+    endOfToken = strstr(request->path, " ");
+    endOfToken[0] = '\0';
+
+    request->version = endOfToken + 1;
+    endOfToken = strstr(request->version, "\r\n");
+    endOfToken[0] = '\0';
+
+    // parse headers
 
     while (keyBegin > (char*) 2 && keyBegin < request->body - 2) {
         char* valBegin = strstr(keyBegin, ": ");
